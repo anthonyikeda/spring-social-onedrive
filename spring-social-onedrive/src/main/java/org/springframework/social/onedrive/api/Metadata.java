9454b04bf74acdeaeb479ed1199224c3da9e72d4
Suresh Kumar <yarlagaddasuresh@gmail.com>
2014-03-17 22:56:52 +0530

    Partial Creation of Metadat domain.

---
+++ spring-social-onedrive/src/main/java/org/springframework/social/onedrive/api/Metadata.java
@@ -0,0 +1,23 @@
+package org.springframework.social.onedrive.api;
+
+import java.util.Date;
+
+public class Metadata {
+
+	private String name;
+	private String description;
+	private String parentId;
+	private int size;
+	private String uploadLocation;
+	private int commentsCount;
+	private boolean commentsEnabled;
+	private boolean isEmbeddable;
+	private int count;
+	private String link;
+	private String type;
+	private Date createdTime;
+	private Date updatedTime;
+	private Date clientUpdatedTime;
+	
+	
+}
